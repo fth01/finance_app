25926f9ea42ae6b93d10d94b9d98ae83e6d38b31
fth01 <fkangal01@gmail.com>
2025-01-12 14:28:07 +0300

    report fix

--- src/test/java/com/app/runners/TestRunner.java
+++ src/test/java/com/app/runners/TestRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {
                 "json:target/cucumber.json",
-                "html:target/default-html-reports",
+                "html:target/default-html-reports.html",
                 "rerun:target/rerun.txt"
         },
         features = "src/test/resources/features",
--- src/test/java/com/app/runners/TestRunnerFailed.java
+++ src/test/java/com/app/runners/TestRunnerFailed.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/failed-html-report"},
+        plugin = {"html:target/failed-html-report.html"},
         features = "@target/rerun.txt",
         glue = "com/app/step_definitions"
 )
